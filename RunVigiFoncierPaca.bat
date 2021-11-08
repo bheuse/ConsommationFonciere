5e8a609edf05e001f18b554657f73b76b7ae1ec1
Bernard <bheuse@gmail.com>
2021-11-08 08:19:02 +0100

    Carte

---
+++ RunVigiFoncierPaca.bat
@@ -0,0 +1,6 @@
+python.exe .\ConsommationFonciere.py -a  -p -d 13
+python.exe .\ConsommationFonciere.py -a  -p -d 84
+python.exe .\ConsommationFonciere.py -a  -p -d 83
+python.exe .\ConsommationFonciere.py -a  -p -d 06
+python.exe .\ConsommationFonciere.py -a  -p -d 04
+python.exe .\ConsommationFonciere.py -a  -p -d 05
