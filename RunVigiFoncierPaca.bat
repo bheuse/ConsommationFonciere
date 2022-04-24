658db887ce52440c63598bd86875d7fc4fd3771a
Bernard Heuse <bheuse@amdocs.com>
2022-04-24 10:57:20 +0200

    V4 Beta

--- RunVigiFoncierPaca.bat
+++ RunVigiFoncierPaca.bat
@@ -1,8 +1,17 @@
-python.exe .\ConsommationFonciere.py -t -p -m
+python.exe .\ConsommationFonciere.py -t -p       -m
 python.exe .\ConsommationFonciere.py -t -f -a -p -d 06
 python.exe .\ConsommationFonciere.py -t -f -a -p -d 13
 python.exe .\ConsommationFonciere.py -t -f -a -p -d 84
 python.exe .\ConsommationFonciere.py -t -f -a -p -d 83
 python.exe .\ConsommationFonciere.py -t -f -a -p -d 04
 python.exe .\ConsommationFonciere.py -t -f -a -p -d 05
-python.exe .\ConsommationFonciere.py -t -f -p -r 93
+python.exe .\ConsommationFonciere.py -t -f    -p -r 93
+
+python.exe .\ConsommationFonciere.py -t          -s -m
+python.exe .\ConsommationFonciere.py -t -f -a -p -s -d 06
+python.exe .\ConsommationFonciere.py -t -f -a -p -s -d 13
+python.exe .\ConsommationFonciere.py -t -f -a -p -s -d 84
+python.exe .\ConsommationFonciere.py -t -f -a -p -s -d 83
+python.exe .\ConsommationFonciere.py -t -f -a -p -s -d 04
+python.exe .\ConsommationFonciere.py -t -f -a -p -s -d 05
+python.exe .\ConsommationFonciere.py -t -f    -p -s -r 93
