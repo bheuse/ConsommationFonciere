570b22d7ceedbb12a8970c5fc6a5c76753d6633f
Bernard Heuse <bheuse@amdocs.com>
2022-11-26 07:37:01 +0100

    V4 Beta

--- RunVigiFoncierPaca.sh
+++ RunVigiFoncierPaca.sh
@@ -1,8 +1,8 @@
-python3.9 ./ConsommationFonciere.py  --clean
-python3.9 ./ConsommationFonciere.py  -p -s -f -a -d 04
-python3.9 ./ConsommationFonciere.py  -p -s -f -a -d 05
-python3.9 ./ConsommationFonciere.py  -p -s -f -a -d 06
-python3.9 ./ConsommationFonciere.py  -p -s -f -a -d 13
-python3.9 ./ConsommationFonciere.py  -p -s -f -a -d 83
-python3.9 ./ConsommationFonciere.py  -p -s -f -a -d 84
-python3.9 ./ConsommationFonciere.py  -p -s -m
+python3.9 ./ConsommationFonciereV4.py  --clean
+python3.9 ./ConsommationFonciereV4.py  -p -s -f -a -d 04
+python3.9 ./ConsommationFonciereV4.py  -p -s -f -a -d 05
+python3.9 ./ConsommationFonciereV4.py  -p -s -f -a -d 06
+python3.9 ./ConsommationFonciereV4.py  -p -s -f -a -d 13
+python3.9 ./ConsommationFonciereV4.py  -p -s -f -a -d 83
+python3.9 ./ConsommationFonciereV4.py  -p -s -f -a -d 84
+python3.9 ./ConsommationFonciereV4.py  -p -s -m
